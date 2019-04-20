d4e45ca343ec8d06b05036f2a57a90234120cd12
Justin <iceyang1007@gmail.com>
2019-04-20 14:15:37 +0800

    Update context.ts

--- lib/context.ts
+++ lib/context.ts
@@ -1,51 +1,6 @@
 import BeanFactory from './bean_factory';
+import { BeanConfig, FieldConfig, PropertyConfig } from './config';
 import DEBUG from 'debug';
-
-export type Constructor = { new(...args:any[]):{} };
-
-export class BeanConfig {
-  private initialConstructor: Constructor;
-
-  constructor(initialConstructor: Constructor) {
-    this.initialConstructor = initialConstructor;
-  }
-
-  newBeanInstance(): object {
-    return new this.initialConstructor();
-  }
-}
-
-export class FieldConfig {
-  private beanName: string;
-  private field: string;
-  private value: any;
-
-  constructor(beanName: string, field: string, value: any) {
-    this.beanName = beanName;
-    this.field = field;
-    this.value = value;
-  }
-
-  getField() { return this.field; }
-
-  getValue() { return this.value; }
-}
-
-export class PropertyConfig {
-  private beanName: string;
-  private property: string;
-  private injectBeanName: string;
-
-  constructor(beanName: string, property: string, injectBeanName: string) {
-    this.beanName = beanName;
-    this.property = property;
-    this.injectBeanName = injectBeanName;
-  }
-
-  getProperty() { return this.property; }
-
-  getInjectBeanName() { return this.injectBeanName; }
-}
 
 export default class ApplicationContext {
   private beanFactory: BeanFactory = new BeanFactory();
@@ -129,4 +84,3 @@ export default class ApplicationContext {
     this.propertyConfigs[beanName].push(propertyConfig);
   }
 }
-
